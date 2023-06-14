e3078cc457f36026093d516db905e72d64243d30
AbmSourav <keramotul.islam@gmail.com>
2023-06-14 16:37:33 +0600

    responsive issue fixed

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,4 +1,6 @@
 import Footer from "./Components/Footer"
+import { useRouter } from 'next/router'
+import { useConfig } from 'nextra-theme-docs'
 
 export default {
     logo: <strong style={{ color: '#06aa06', fontSize: '30px' }}>Kathamo</strong>,
@@ -14,14 +16,29 @@ export default {
 		content: 'Feedback',
 		useLink: () => 'https://github.com/CodesVault/kathamo-doc/issues'
 	},
+	head: () => {
+		const { asPath } = useRouter()
+		const { title } = useConfig()
+		return <>
+			<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+			<meta property="og:url" content={`https://kathamo.dev${asPath}`} />
+			<meta property="og:title" content={`${title} | Kathamo` || 'Kathamo'} />
+			<meta property="twitter:title" content={`${title} | Kathamo` || 'Kathamo'} />
+			<meta property="og:description" content="Framework for WordPress plugin development. It helps you to develop maintainable and scalable plugin." />
+			<meta property="twitter:description" content="Framework for WordPress plugin development. It helps you to develop maintainable and scalable plugin." />
+			<meta property="og:type" content="documentation" />
+			<meta property="twitter:card" content="documentation" />
+			<meta property='og:image' content='//abmsourav.com/welcome/wp-content/uploads/2023/06/Kathamo-framework.png' />
+			<meta property='twitter:image' content='//abmsourav.com/welcome/wp-content/uploads/2023/06/Kathamo-framework.png' />
+	  </>
+	},
 	footer: {
-		// text: <span>Copyright © 2023 <a href="https://github.com/CodesVault" target="_blank">CodesVault</a></span>,
 		text: <Footer />,
 	},
-	banner: {
-		key: '1.0.0-release',
-		text: 'Kathamo 1.0.0 is released.',
-	},
+	// banner: {
+	// 	key: '1.0.0-release',
+	// 	text: 'Kathamo 1.0.0 is released.',
+	// },
 	sidebar: {
 		defaultMenuCollapseLevel: 1
 	  },
